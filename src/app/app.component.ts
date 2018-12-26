0e3bea94ea072568e10ab7063c1f1dd1c9a4bf3e
Ramesh Gurung <rgurung@growbydata.com>
2018-12-26 09:30:38 +0545

    angular todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+	title = 'Todo App';
+
+  // todos = ['todo1', 'todo2'];
+
+  todo = {
+      "key" : 0,
+      "description" : "",
+      "done" : 0,
+      "name" : ""
+  };
+
+  todos = [
+      {
+        "key" : 1,
+    		"description" : "description1",
+    		"done" : 0,
+    		"name" : "n1"
+    	},
+      {
+        "key" : 2,
+    		"description" : "description2",
+    		"done" : 1,
+    		"name" : "n2"
+    	},
+      {
+        "key" : 3,
+        "description" : "description3",
+        "done" : 0,
+        "name" : "n3"
+      }
+
+    ];
+
+  	todo_save(){
+      if(this.todo.key){
+        console.log('update')
+        //this.todos.
+      }else{
+        this.todos.push(this.todo);
+        console.log('add');      
+      }
+      // reset todo
+      this._reset_todo();
+  	}
+
+  	todo_edit(todo){
+      this.todo = todo;
+  	}
+
+  	todo_done(todo){
+      this.todo = todo;
+      this.todo.done = 1;
+      console.log('done: ', this.todo);
+      // reset todo
+      this._reset_todo();
+  	}
+
+  	todo_delete(todo_key){
+      this.todos = this._find(todo_key);
+      // reset todo
+      this._reset_todo();
+  	}
+
+    _find(todo_key){
+      return this.todos.filter(function(currentValue){
+        return currentValue.key != todo_key;
+      },todo_key); 
+    }
+
+    _reset_todo(){
+      this.todo = {
+          "key" : 0,
+          "description" : "",
+          "done" : 0,
+          "name" : ""
+      };
+    }
+
+  }
